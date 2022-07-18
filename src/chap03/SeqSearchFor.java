f611fca7bd19cb05e8dadac4991af4d7953da678
Inssg <rnlrhdwk98@gmail.com>
2022-07-18 22:10:26 +0900

    SeqSearch

---
+++ src/chap03/SeqSearchFor.java
@@ -0,0 +1,10 @@
+package chap03;
+
+public class SeqSearchFor {
+    static int seqSearch(int[] a, int n, int key)    {
+        for (int i = 0; i < n; i++)
+            if(a[i] == key)
+                return i;
+            return -1;
+    }
+}
