9edfe1ca55673875668d1cd0376e9781ddb5d41d
Inssg <101978825+Inssg@users.noreply.github.com>
2022-12-24 14:44:52 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int i = Integer.parseInt(br.readLine());
+        String s = "*";
+        String s1 = " ";
+        for (int k = 1; k <= i; k++) {
+            sb.append(s1.repeat(i-k)).append(s.repeat(k)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
