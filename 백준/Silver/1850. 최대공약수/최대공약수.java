5895b35e9fa0d75e00c427936b6f6ebce67a194c
Inssg <101978825+Inssg@users.noreply.github.com>
2023-01-16 21:01:01 +0900

    [Silver I] Title: 최대공약수, Time: 308 ms, Memory: 38788 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        long gcd = gcd(a, b);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < gcd; i++) {
+            sb.append("1");
+        }
+        System.out.println(sb);
+
+    }
+
+    static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    }
+
+
+
+
+
