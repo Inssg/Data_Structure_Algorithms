f1588b918574a1f5009c4503110927562c5c97a7
Inssg <101978825+Inssg@users.noreply.github.com>
2023-04-03 17:59:02 +0900

    [level 2] Title: 소수 찾기, Time: 14.22 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    boolean[] check = new boolean[7];
+    ArrayList<Integer> arr = new ArrayList<>();
+    public int solution(String numbers) {
+        int answer = 0;
+        for(int i = 0; i < numbers.length(); i++){
+            dfs(numbers, "", i+1);
+        }
+        
+        for(int i = 0; i < arr.size(); i++){
+           if(prime(arr.get(i))) answer++; 
+        }
+        return answer;
+    }
+    
+    void dfs(String str, String temp, int m){
+        if(temp.length() == m){
+            int num = Integer.parseInt(temp);
+            if(!arr.contains(num)){
+                arr.add(num);
+            }
+            return;
+        }
+        
+        for(int i = 0; i <str.length(); i++){
+            if(!check[i]){
+                check[i] = true;
+                temp += str.charAt(i);
+                dfs(str, temp, m);
+                check[i] = false;
+                temp = temp.substring(0, temp.length() -1);
+            }
+        }
+    }
+    static boolean prime(int n) {
+        if(n < 2) return false;
+        
+        for(int i =2; i*i <= n; i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+}
